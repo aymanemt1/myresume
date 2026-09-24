9064e02890e94209ce4d67df67a774288793d5eb
moutoute Aymane <114621164+aymanemt1@users.noreply.github.com>
2026-09-24 17:27:51 +0100

    Add MySkills component to Home page

--- portfolio/src/Master/Home.js
+++ portfolio/src/Master/Home.js
@@ -1,17 +1,20 @@
-
 import React from "react";
 import "./home.css";
 
 import { Content } from "../Composant/Content/Content";
 import About from "../Composant/About/About";
+import { MySkills } from "../Composant/Skills/Skills";
 
 export const Home = () => {
   return (
     <div id="homeParent">
       <Content />
+
       <div id="aboutParent">
         <About />
       </div>
+
+      <MySkills />
     </div>
   );
 };
