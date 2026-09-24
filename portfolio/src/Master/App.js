12998804a7f79558b6c4eb7d595d2451751d75a8
moutoute Aymane <114621164+aymanemt1@users.noreply.github.com>
2026-09-24 18:12:40 +0100

    Wrap Home component in LangueProvider

--- portfolio/src/Master/App.js
+++ portfolio/src/Master/App.js
@@ -1,6 +1,11 @@
-import React from "react";
-import { Home } from "./Home";
+import React from 'react';
+import { Home } from './Home';
+import { LangueProvider } from '../Context/LangueContext';
 
 export const App = () => {
-  return <Home />;
+  return (
+    <LangueProvider>
+      <Home />
+    </LangueProvider>
+  );
 };
