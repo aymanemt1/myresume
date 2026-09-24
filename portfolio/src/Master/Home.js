367596ef81a62a7dfd9fde4910a0d94bb5cc2bab
moutoute Aymane <114621164+aymanemt1@users.noreply.github.com>
2026-09-24 17:21:54 +0100

    Update Home.js

--- portfolio/src/Master/Home.js
+++ portfolio/src/Master/Home.js
@@ -1,44 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import "./home.css"
-import { Cursor } from '../Composant/Cursor/Cursor'
-import { TopBar } from '../Composant/Topbar/TopBar'
-import ScrollProgressBar from '../Composant/Progress Bar/ScrollProgressBar'
-import Services from '../Composant/Services/Services'
-import About from '../Composant/About/About'
-import { Content } from '../Composant/Content/Content'
-import {Contact} from '../Composant/Contact/Contact'
-import { MySkills } from '../Composant/Skills/Skills'
-import ScrollButton from '../Composant/BtnTop/scrollToTop'
-import { Footer } from '../Composant/Footer/Footer'
-import { Project } from '../Composant/Projects/Project'
+import React from "react";
+import "./home.css";
+
 export const Home = () => {
-
-  const [scrollWidth, setScrollWidth] = useState(0);
-
-  const handleScrollWidth = (width) => {
-    setScrollWidth(width);
-  };
   return (
-    <div id='homeParent'>
-      <ScrollProgressBar onScrollWidthChange={handleScrollWidth} />
-      <Cursor />
-      <TopBar scrollWidth={scrollWidth} />
-      <Content />
-     <div id='aboutParent'>
-     <About />
-     </div>
-      <MySkills />
-      <div id='servicesParent'>
-      <Services />
-      </div>
-      <ScrollButton />
-      <div id='projectsPranet'>
-      <Project />
-      </div>
-      <div id='ContactParent'>
-      <Contact />
-      </div>
-      <Footer />
+    <div
+      id="homeParent"
+      style={{
+        minHeight: "100vh",
+        background: "#ffffff",
+        color: "#000000",
+        padding: "50px",
+      }}
+    >
+      <h1>Aymane Moutoute</h1>
+      <h2>Web Developer</h2>
+      <p>Home component is working correctly.</p>
     </div>
-  )
-}
+  );
+};
