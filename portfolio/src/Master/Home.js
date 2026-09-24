559a71b6b0cd8aa0f601cd96ff182cc79ac0a1f7
moutoute Aymane <114621164+aymanemt1@users.noreply.github.com>
2026-09-24 17:25:03 +0100

    Update Home.js

--- portfolio/src/Master/Home.js
+++ portfolio/src/Master/Home.js
@@ -1,20 +1,12 @@
 import React from "react";
 import "./home.css";
 
+import { Content } from "../Composant/Content/Content";
+
 export const Home = () => {
   return (
-    <div
-      id="homeParent"
-      style={{
-        minHeight: "100vh",
-        background: "#ffffff",
-        color: "#000000",
-        padding: "50px",
-      }}
-    >
-      <h1>Aymane Moutoute</h1>
-      <h2>Web Developer</h2>
-      <p>Home component is working correctly.</p>
+    <div id="homeParent">
+      <Content />
     </div>
   );
 };
