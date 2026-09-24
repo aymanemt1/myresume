7545262f4e8d9e3dda1678875570513609c54077
moutoute Aymane <114621164+aymanemt1@users.noreply.github.com>
2026-09-24 18:03:24 +0100

    Refactor index.js for consistent quote style
    Updated import statements to use single quotes and removed unnecessary lines.

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,17 +1,14 @@
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './Master/App';
 import { LangueProvider } from "./Context/LangueContext";
-import { App } from "./Master/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <LangueProvider>
-      <App />
-    </LangueProvider>
-  </React.StrictMode>
+  <LangueProvider>
+    <App />
+  </LangueProvider>
 );
 
