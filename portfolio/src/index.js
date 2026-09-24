5f1fa7c103be4f4691773477741ccfcb3d2ea0f1
moutoute Aymane <114621164+aymanemt1@users.noreply.github.com>
2026-09-24 18:06:16 +0100

    Remove unused index.css import
    Removed the import statement for index.css.

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,7 +1,6 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { App } from "./Master/App";
 import { LangueProvider } from "./Context/LangueContext";
 
