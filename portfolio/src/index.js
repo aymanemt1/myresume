9292bc2f3b705d7249d4acfc68551e292f48a46b
moutoute Aymane <114621164+aymanemt1@users.noreply.github.com>
2026-09-24 18:04:35 +0100

    Change single quotes to double quotes in index.js
    Updated quotes from single to double in index.js.

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { App } from './Master/App';
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "./Master/App";
 import { LangueProvider } from "./Context/LangueContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <LangueProvider>
     <App />
   </LangueProvider>
 );
-
