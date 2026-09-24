b3eef69be8769e3effb0895d192fa8cbe5c39691
moutoute Aymane <114621164+aymanemt1@users.noreply.github.com>
2026-09-24 17:26:26 +0100

    Add About component to Home page

--- portfolio/src/Master/Home.js
+++ portfolio/src/Master/Home.js
@@ -1,12 +1,17 @@
+
 import React from "react";
 import "./home.css";
 
 import { Content } from "../Composant/Content/Content";
+import About from "../Composant/About/About";
 
 export const Home = () => {
   return (
     <div id="homeParent">
       <Content />
+      <div id="aboutParent">
+        <About />
+      </div>
     </div>
   );
 };
